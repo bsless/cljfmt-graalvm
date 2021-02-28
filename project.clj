ca94b32827fabc492f15708c6aac90d6e244751a
Kanwei Li <kanwei@gmail.com>
2021-02-28 11:29:04 -0500

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [cljfmt "0.6.6"]]
+  :dependencies [[org.clojure/clojure "1.10.2"]
+                 [cljfmt "0.7.0"]]
   :global-vars {#_#_*assert* false
                 #_#_*warn-on-reflection* true}
   :main cljfmt-graalvm.core
